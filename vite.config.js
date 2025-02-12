cd2a640e445f9cefc206d3af8c270583e509d7f2
Camilo <110619742+CamiloMVJ@users.noreply.github.com>
2025-02-11 21:26:24 -0600

    actualizacion

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://phonebook-rq3k.onrender.com',
+  base: 'https://phonebook-0ume.onrender.com',
   plugins: [react()],
 })
